55d2a29c09f8b36eaf3b9916c98e793566fe71a1
Carlie Smith <carliesmith011@gmail.com>
2022-04-23 16:10:29 -0400

    first commit with source code files

---
+++ src/components/Footer/index.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+
+function Footer () {
+      
+
+      
+  return (
+    <footer className='flex-row px-1'>
+        CJSmith011   
+          <link src="https://github.com/cjsmith011">Github</link>
+          <link src="https://www.linkedin.com/feed/?trk=homepage-basic_signin-form_submit">linkedin</link>
+    </footer>
+
+  )
+}
+
+export default Footer;
---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
+
+import PhotoList from './Photolist';
+
+function Gallery({currentCategory}) {
+    const { name, description } = currentCategory;
+    
+    return (
+        <section>
+            <h1>{capitalizeFirstLetter(name)}</h1>
+            <p>{description}</p>
+            <PhotoList category={currentCategory.name}  />
+        </section>
+  );
+}
+export default Gallery;
